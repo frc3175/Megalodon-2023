72ae9ea232bab26daa6db5875aaef8bbe19b7fa5
Isabella Haladjian <isabellahaladjian@uls.net>
2023-02-09 15:45:02 -0500

    Added auto util class and eventMap

--- src/main/java/frc/robot/autos/automodes/Auto.java
+++ src/main/java/frc/robot/autos/automodes/Auto.java
@@ -6,6 +6,11 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
+import frc.robot.commands.SetIntake;
+import frc.robot.commands.SetOuttake;
+import frc.robot.commands.SetRobotStateHigh;
+import frc.robot.commands.SetRobotStateLow;
+import frc.robot.commands.SetRobotStateMid;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -14,11 +19,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Auto {
+
   private static final Map<String, Command> eventMap = new HashMap<>(Map.ofEntries(
           Map.entry("Stop", new InstantCommand(RobotContainer.m_drivetrain::stopSwerve)),
-          Map.entry("example2", Commands.print("Example 2 triggered")),
-          Map.entry("example3", Commands.print("Example 3 triggered"))
+          Map.entry("CubeMode", new InstantCommand(() -> RobotContainer.m_robotState.setGamepieceState(false))),
+          Map.entry("ConeMode", new InstantCommand(() -> RobotContainer.m_robotState.setGamepieceState(true))),
+          Map.entry("High", new SetRobotStateHigh(RobotContainer.m_robotState, RobotContainer.m_intake)),
+          Map.entry("Mid", new SetRobotStateMid(RobotContainer.m_robotState, RobotContainer.m_intake)),
+          Map.entry("Low", new SetRobotStateLow(RobotContainer.m_robotState, RobotContainer.m_intake)),
+          Map.entry("Intake", new SetIntake(RobotContainer.m_intake, RobotContainer.m_robotState)),
+          Map.entry("Outtake", new SetOuttake(RobotContainer.m_intake, RobotContainer.m_robotState))
   ));
+
   private static final SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
           RobotContainer.m_drivetrain::getPose,
           RobotContainer.m_drivetrain::resetOdometry,
